ef5d3b90798f3a13628a8335d584b779b6a0fdfc
Wambugu Kinyua <95144881+wambugu71@users.noreply.github.com>
2024-01-06 22:43:20 +0300

    Update ken_dtale.py

--- ken_dtale.py
+++ ken_dtale.py
@@ -3,6 +3,6 @@ import  dtale
 import  streamlit as  st
 filename = "auto-mpg.csv"
 df = pd.read_csv(filename).head()
-d = dtale.show(df)
+d = dtale.show(df, port = 5000)
 st.write("dtale  demo")
 st.write(" <a href='http://wambugu:40000/dtale/main/1' target='_blank'>Wambugu Kinyua</a>", unsafe_allow_html =True)
